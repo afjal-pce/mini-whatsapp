94ed2d289e69e029741b0c4e100f0666af1eb17f
afjal-pce <afjalansari853941@gmail.com>
2026-02-16 22:40:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,18 +3,36 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
+// Home page
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+// 🔥 SOCKET CONNECTION
 io.on("connection", (socket) => {
   console.log("User connected");
 
+  // Receive message
   socket.on("chat message", (msg) => {
     io.emit("chat message", msg);
+
+    // 🔥 AUTO REPLY LOGIC
+    if (msg.toLowerCase() === "rahul") {
+      io.emit("chat message", "Hello Rahul 👋");
+    }
+
+    if (msg.toLowerCase() === "hello") {
+      io.emit("chat message", "Hi there! 😊");
+    }
+
+    if (msg.toLowerCase() === "bye") {
+      io.emit("chatak message", "Goodbye 👋");
+    }
   });
 });
 
-http.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+// Server start
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
