a58b4f070ebdf628f4062ab09ee9ea0dafda7c9e
afjal-pce <afjalansari853941@gmail.com>
2026-02-16 20:06:54 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+http.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
